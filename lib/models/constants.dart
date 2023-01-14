a3e218219b945300ae060d3b2e90c0edbc65e8d3
imtheguna <58139175+imtheguna@users.noreply.github.com>
2023-01-14 15:57:24 +0530

    Update constants.dart

--- lib/models/constants.dart
+++ lib/models/constants.dart
@@ -1,8 +1,8 @@
 import 'package:docs/models/chatgptmodel.dart';
 import 'package:flutter/material.dart';
 
-//String host = 'http://192.168.1.2:3001';
-String chatGPTAPI = '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';
+//String host = '';
+String chatGPTAPI = '';
 List<ChatGPTMsgModel> chatlist = [];
 String host = 'https://gk-docs.onrender.com';
 
